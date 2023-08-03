52c36e63da7d94d8c2b8eaee6340e38c72b38d72
Yuchao Liao <yuchaoliao@email.arizona.edu>
2023-08-03 11:40:31 -0700

    Init

---
+++ Metrics.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import scipy.linalg as linalg
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#FID
+from torchvision.transforms import ToTensor
+from torchvision.models import inception_v3
+from torch.nn.functional import adaptive_avg_pool2d
+
+#MMD metric
+def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+
+    n_samples = int(source.size()[0])+int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    
+    total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    
+    L2_distance = ((total0-total1)**2).sum(2) 
+    
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+    
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+    
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    
+    return sum(kernel_val)#/len(kernel_val)
+
+def mmd_rbf(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+   
+    batch_size = int(source.size()[0])
+    kernels = guassian_kernel(source, target,
+        kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    
+    XX = kernels[:batch_size, :batch_size]
+    YY = kernels[batch_size:, batch_size:]
+    XY = kernels[:batch_size, batch_size:]
+    YX = kernels[batch_size:, :batch_size]
+    loss = torch.mean(XX + YY - XY -YX)
+    return loss
+
+#SSD metric
+def SSD(y, y_pred):
+    return np.sum(np.square(y - y_pred), axis=1)  # axis 1 is the signal dimension
+
+#PRD metric
+def PRD(y, y_pred):
+    N = np.sum(np.square(y_pred - y), axis=1)
+    D = np.sum(np.square(y_pred - np.mean(y)), axis=1)
+
+    PRD = np.sqrt(N/D) * 100
+
+    return PRD
+
+#COSS metric
+def COSS(y, y_pred):
+    return torch.nn.functional.cosine_similarity(torch.tensor(y),torch.tensor(y_pred))
